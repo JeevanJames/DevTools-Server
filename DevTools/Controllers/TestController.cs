3cbd92e3b60c5d3fb6ded0a1e7a3b2be084dedfa
Jeevan James <jeevanjj@yahoo.com>
2016-05-18 00:40:57 +0530

    Added solution and Web API project.

---
+++ DevTools/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+
+namespace DevTools.Controllers
+{
+    [RoutePrefix("api")]
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("test/add/{x:int}/{y:int}")]
+        public IHttpActionResult Add(int x, int y)
+        {
+            return Ok(new { Sum = x + y });
+        }
+    }
+}
---
+++ DevTools/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+
+using DevTools;
+
+using Microsoft.Owin;
+
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace DevTools
+{
+    public sealed class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpServer server = CreateWebApiConfig();
+            app.UseWebApi(server);
+        }
+
+        private HttpServer CreateWebApiConfig()
+        {
+            var config = new HttpConfiguration();
+            var server = new HttpServer(config);
+
+            config.MapHttpAttributeRoutes();
+
+            return server;
+        }
+    }
+}
